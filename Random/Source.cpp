9561ffe248b253e604fec18ff3f0a4f5e39bc721
KateKrechet <k.krechet@yandex.ru>
2021-07-13 22:45:20 +0300

    +selection sort

--- Random/Source.cpp
+++ Random/Source.cpp
@@ -20,7 +20,28 @@ void main()
 	{
 		arr[i] = rand() % (maxRand - minRand) + minRand;
 	}
+	//вывод исходного массива на экран
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
 
+	//сортировка выбором
+	for (int i = 0; i < n; i++) //выбирает элемент, в который нужно поместить минимальный элемент
+	{
+		for (int j = i + 1; j < n; j++)
+			if (arr[j] < arr[i]) //если перебираемый меньше выбранного элемента
+			{
+				int buffer = arr[i];
+				arr[i] = arr[j];
+				arr[j] = buffer;
+
+			}
+
+	}
+
+	//вывод отсортированного массива на экран
 	for (int i = 0; i < n; i++)
 	{
 		cout << arr[i] << "\t";
